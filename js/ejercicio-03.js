52cb3913fe3d57b12b66175cb59a8e759988c260
Rodrigo Rueda <rodrigo21492@gmail.com>
2026-03-08 17:34:48 -0300

    listo ejercicio 3

---
+++ js/ejercicio-03.js
@@ -0,0 +1,4 @@
+//Escribe un programa de una sola línea que escriba en la pantalla el resultado de sumar 3 + 5.
+
+const texto = document.getElementById('pantalla')
+texto.innerHTML = "<p>" + (3+5) + "</p>"
